2b6318be35a79d268dd1b5d91aac357114da8275
Tomislav Jakupić <tojaku@gmail.com>
2024-10-13 20:32:32 +0200

    playing with html editor

--- src/pages/settings/News.jsx
+++ src/pages/settings/News.jsx
@@ -1,7 +1,40 @@
+import { onMount } from "solid-js";
 import CollectionEditor from "../../components/CollectionEditor";
 import FormButtons from "../../components/FormButtons";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
 export default function SettingsNews(props) {
+    let quillContainer;
+    let quillTextarea;
+
+    onMount(() => {
+        const quill = new Quill(quillContainer, {
+            theme: "snow",
+            modules: {
+                toolbar: [
+                    [{ header: [1, 2, false] }],
+                    ["bold", "italic", "underline"],
+                    ["image", "code-block"]
+                ]
+            }
+        });
+        quill.on("text-change", () => {
+            quillTextarea.value = quill.root.innerHTML;
+        });
+        const observer = new MutationObserver(() => {
+            quillContainer.innerHTML = quillTextarea.value;
+            console.log("updated");
+            
+        });
+        observer.observe(quillTextarea, {
+            attributes: true,
+            childList: true,
+            subtree: true,
+            characterData: true
+        })
+    });
+
     return (
         <>
             <div class="prose mb-8">
@@ -20,9 +53,9 @@ export default function SettingsNews(props) {
                         <label class="label">
                             <span class="label-text">Sadržaj</span>
                         </label>
-                        <textarea name="content" class="textarea textarea-bordered" required="" rows={10}></textarea>
+                        <div ref={quillContainer} class="quill-editor h-60"></div>
+                        <textarea name="content" ref={quillTextarea}></textarea>
                     </div>
-
                     <FormButtons />
                 </form>
             </CollectionEditor>
