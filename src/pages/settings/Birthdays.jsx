69981bba63611a182e78001ef899b0b5e0753687
Tomislav Jakupic <tojaku@gmail.com>
2024-10-17 08:38:57 +0200

    birthday month bug fix

--- src/pages/settings/Birthdays.jsx
+++ src/pages/settings/Birthdays.jsx
@@ -61,7 +61,7 @@ export default function SettingsBirthdays(props) {
                         break;
                     } else {
                         let day = parseInt(dobStr.substring(0, 2));
-                        let month = parseInt(dobStr.substring(3, 5)) - 1;
+                        let month = parseInt(dobStr.substring(3, 5));
                         let year = parseInt(dobStr.substring(6, 10));
                         // let dob = new Date(year, month, day, 0, 0, 0, 0)
                         items.push({ day, month, year, name: `${name} ${surname}`, group: `${classNumber}.${classLabel}`, user: user().id });
